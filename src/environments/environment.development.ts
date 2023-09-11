8c6f6e7b4332864782e4a08daccdb2bc6f82de3a
Aditya Pradeep <71806984+aditya-130@users.noreply.github.com>
2023-09-11 19:55:08 +0530

    setup-initial-commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase:{
+        apiKey: "AIzaSyAuHx2AdKUVvMt1Tp3GfTQh3dvFLZmc654",
+        authDomain: "dummy-project-83946.firebaseapp.com",
+        projectId: "dummy-project-83946",
+        storageBucket: "dummy-project-83946.appspot.com",
+        messagingSenderId: "334765384724",
+        appId: "1:334765384724:web:5733745757c4f4630cf301",
+        measurementId: "G-FXV7DJ4ZMP"
+    }
+};
